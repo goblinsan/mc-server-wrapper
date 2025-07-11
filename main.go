ec369f5eb04d78de393dceaafc53f4d4184cfa9e
goblinsan <coghlanjames@yahoo.com>
2025-07-11 18:04:23 -0400

    config path flag

--- main.go
+++ main.go
@@ -1,22 +1,23 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"os"
+    "flag"
+    "fmt"
+    "log"
 
 	"github.com/goblinsan/mc-server-wrapper/config"
 )
 
 func main() {
-	configPath := "config.json" // or use a flag/env var for flexibility
-	if len(os.Args) > 1 {
-		configPath = os.Args[1]
-	}
-	cfg, err := config.LoadConfig(configPath)
-	if err != nil {
-		log.Fatalf("Failed to load config: %v", err)
-	}
-	fmt.Printf("Loaded config: %+v\n", cfg)
-	// ...rest of your app logic...
+    // Allow config file path as a command-line argument
+    configPath := flag.String("config", "config.json", "Path to config file")
+    flag.Parse()
+
+    cfg, err := config.LoadConfig(*configPath)
+    if err != nil {
+        log.Fatalf("Failed to load config: %v", err)
+    }
+
+    fmt.Printf("Loaded config: %+v\n", cfg)
+    // ...rest of your app logic...
 }
